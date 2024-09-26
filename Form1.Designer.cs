7c729be94913d7ee6ffb2001be4046354d905ac9
Marcio <marciopalazzo22@gmail.com>
2024-09-26 10:16:50 -0300

    Inicio proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,496 @@
+﻿namespace Bitacora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            label8 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            button3 = new Button();
+            button2 = new Button();
+            label7 = new Label();
+            comboBox5 = new ComboBox();
+            textBox2 = new TextBox();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            comboBox4 = new ComboBox();
+            label3 = new Label();
+            comboBox3 = new ComboBox();
+            label2 = new Label();
+            comboBox2 = new ComboBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            monthCalendar1 = new MonthCalendar();
+            button1 = new Button();
+            tabPage2 = new TabPage();
+            label9 = new Label();
+            numericUpDown2 = new NumericUpDown();
+            label10 = new Label();
+            comboBox6 = new ComboBox();
+            textBox3 = new TextBox();
+            label11 = new Label();
+            textBox4 = new TextBox();
+            label12 = new Label();
+            label13 = new Label();
+            comboBox7 = new ComboBox();
+            label15 = new Label();
+            comboBox9 = new ComboBox();
+            label16 = new Label();
+            comboBox10 = new ComboBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1231, 450);
+            tabControl1.TabIndex = 22;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = Color.WhiteSmoke;
+            tabPage1.Controls.Add(label8);
+            tabPage1.Controls.Add(numericUpDown1);
+            tabPage1.Controls.Add(button3);
+            tabPage1.Controls.Add(button2);
+            tabPage1.Controls.Add(label7);
+            tabPage1.Controls.Add(comboBox5);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(label6);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(comboBox4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(comboBox3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(comboBox2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(comboBox1);
+            tabPage1.Controls.Add(monthCalendar1);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1226, 410);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Home";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(475, 220);
+            label8.Name = "label8";
+            label8.Size = new Size(72, 15);
+            label8.TabIndex = 41;
+            label8.Text = "Tiempo (Hs)";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(477, 237);
+            numericUpDown1.Maximum = new decimal(new int[] { 24, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 40;
+            numericUpDown1.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // button3
+            // 
+            button3.Location = new Point(102, 227);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 39;
+            button3.Text = "Consultar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(328, 334);
+            button2.Name = "button2";
+            button2.Size = new Size(162, 23);
+            button2.TabIndex = 38;
+            button2.Text = "Ver excel";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(477, 47);
+            label7.Name = "label7";
+            label7.Size = new Size(85, 15);
+            label7.TabIndex = 37;
+            label7.Text = "Requerimiento";
+            // 
+            // comboBox5
+            // 
+            comboBox5.FormattingEnabled = true;
+            comboBox5.Location = new Point(480, 71);
+            comboBox5.Name = "comboBox5";
+            comboBox5.Size = new Size(121, 23);
+            comboBox5.TabIndex = 36;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(477, 183);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(286, 23);
+            textBox2.TabIndex = 35;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(477, 165);
+            label6.Name = "label6";
+            label6.Size = new Size(84, 15);
+            label6.TabIndex = 34;
+            label6.Text = "Observaciones";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(477, 127);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(286, 23);
+            textBox1.TabIndex = 33;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(477, 109);
+            label5.Name = "label5";
+            label5.Size = new Size(83, 15);
+            label5.TabIndex = 32;
+            label5.Text = "Tarea realizada";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(328, 219);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 15);
+            label4.TabIndex = 31;
+            label4.Text = "Modulo";
+            // 
+            // comboBox4
+            // 
+            comboBox4.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(328, 237);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(121, 23);
+            comboBox4.TabIndex = 30;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(328, 53);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 29;
+            label3.Text = "Recurso";
+            // 
+            // comboBox3
+            // 
+            comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(328, 71);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(121, 23);
+            comboBox3.TabIndex = 28;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(328, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 27;
+            label2.Text = "Banco";
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(328, 183);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 26;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(328, 109);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 15);
+            label1.TabIndex = 25;
+            label1.Text = "Tipo de Tarea";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Analisis", "Desarrollo", "Pruebas Unitarias", "Testing" });
+            comboBox1.Location = new Point(328, 127);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(118, 23);
+            comboBox1.TabIndex = 24;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.BackColor = SystemColors.Window;
+            monthCalendar1.Location = new Point(34, 53);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 23;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(328, 290);
+            button1.Name = "button1";
+            button1.Size = new Size(162, 23);
+            button1.TabIndex = 22;
+            button1.Text = "Registrar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = Color.WhiteSmoke;
+            tabPage2.Controls.Add(label9);
+            tabPage2.Controls.Add(numericUpDown2);
+            tabPage2.Controls.Add(label10);
+            tabPage2.Controls.Add(comboBox6);
+            tabPage2.Controls.Add(textBox3);
+            tabPage2.Controls.Add(label11);
+            tabPage2.Controls.Add(textBox4);
+            tabPage2.Controls.Add(label12);
+            tabPage2.Controls.Add(label13);
+            tabPage2.Controls.Add(comboBox7);
+            tabPage2.Controls.Add(label15);
+            tabPage2.Controls.Add(comboBox9);
+            tabPage2.Controls.Add(label16);
+            tabPage2.Controls.Add(comboBox10);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1223, 422);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Tareas del dia";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(10, 41);
+            label9.Name = "label9";
+            label9.Size = new Size(72, 15);
+            label9.TabIndex = 57;
+            label9.Text = "Tiempo (Hs)";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(12, 58);
+            numericUpDown2.Maximum = new decimal(new int[] { 24, 0, 0, 0 });
+            numericUpDown2.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(112, 23);
+            numericUpDown2.TabIndex = 56;
+            numericUpDown2.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(382, 39);
+            label10.Name = "label10";
+            label10.Size = new Size(85, 15);
+            label10.TabIndex = 55;
+            label10.Text = "Requerimiento";
+            // 
+            // comboBox6
+            // 
+            comboBox6.FormattingEnabled = true;
+            comboBox6.Location = new Point(382, 57);
+            comboBox6.Name = "comboBox6";
+            comboBox6.Size = new Size(121, 23);
+            comboBox6.TabIndex = 54;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(929, 59);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(286, 23);
+            textBox3.TabIndex = 53;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(929, 41);
+            label11.Name = "label11";
+            label11.Size = new Size(84, 15);
+            label11.TabIndex = 52;
+            label11.Text = "Observaciones";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(508, 57);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(286, 23);
+            textBox4.TabIndex = 51;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(508, 39);
+            label12.Name = "label12";
+            label12.Size = new Size(83, 15);
+            label12.TabIndex = 50;
+            label12.Text = "Tarea realizada";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(800, 40);
+            label13.Name = "label13";
+            label13.Size = new Size(49, 15);
+            label13.TabIndex = 49;
+            label13.Text = "Modulo";
+            // 
+            // comboBox7
+            // 
+            comboBox7.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox7.FormattingEnabled = true;
+            comboBox7.Location = new Point(800, 58);
+            comboBox7.Name = "comboBox7";
+            comboBox7.Size = new Size(121, 23);
+            comboBox7.TabIndex = 48;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(130, 39);
+            label15.Name = "label15";
+            label15.Size = new Size(40, 15);
+            label15.TabIndex = 45;
+            label15.Text = "Banco";
+            // 
+            // comboBox9
+            // 
+            comboBox9.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox9.FormattingEnabled = true;
+            comboBox9.Location = new Point(130, 57);
+            comboBox9.Name = "comboBox9";
+            comboBox9.Size = new Size(121, 23);
+            comboBox9.TabIndex = 44;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(257, 39);
+            label16.Name = "label16";
+            label16.Size = new Size(76, 15);
+            label16.TabIndex = 43;
+            label16.Text = "Tipo de Tarea";
+            // 
+            // comboBox10
+            // 
+            comboBox10.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox10.FormattingEnabled = true;
+            comboBox10.Items.AddRange(new object[] { "Analisis", "Desarrollo", "Pruebas Unitarias", "Testing" });
+            comboBox10.Location = new Point(257, 57);
+            comboBox10.Name = "comboBox10";
+            comboBox10.Size = new Size(118, 23);
+            comboBox10.TabIndex = 42;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Menu;
+            ClientSize = new Size(1231, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label8;
+        private NumericUpDown numericUpDown1;
+        private Button button3;
+        private Button button2;
+        private Label label7;
+        private ComboBox comboBox5;
+        private TextBox textBox2;
+        private Label label6;
+        private TextBox textBox1;
+        private Label label5;
+        private Label label4;
+        private ComboBox comboBox4;
+        private Label label3;
+        private ComboBox comboBox3;
+        private Label label2;
+        private ComboBox comboBox2;
+        private Label label1;
+        private ComboBox comboBox1;
+        private MonthCalendar monthCalendar1;
+        private Button button1;
+        private Label label9;
+        private NumericUpDown numericUpDown2;
+        private Label label10;
+        private ComboBox comboBox6;
+        private TextBox textBox3;
+        private Label label11;
+        private TextBox textBox4;
+        private Label label12;
+        private Label label13;
+        private ComboBox comboBox7;
+        private Label label15;
+        private ComboBox comboBox9;
+        private Label label16;
+        private ComboBox comboBox10;
+    }
+}
